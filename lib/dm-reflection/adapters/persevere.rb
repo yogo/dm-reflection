7c5faf25aa8f2610f6efdee4ca7d00f4c331077c
irjudson <irjudson@gmail.com>
2010-01-27 06:14:06 -0700

    Integrated our reflection code.
    This is pretty simplistic code with lots of work to do, but it's functional for now.

--- lib/dm-reflection/adapters/persevere.rb
+++ lib/dm-reflection/adapters/persevere.rb
@@ -1,21 +1,66 @@
 module DataMapper
   module Reflection
     module PersevereAdapter
-      RESERVED_CLASSNAMES = ['User','Transaction','Capability','File','Class']
-
-      def fetch_models
-        @schemas = JSON.parse(self.get_schema)
-        @schemas.map { |schema| schema['id'] unless RESERVED_CLASSNAMES.include?(schema['id']) }.compact
+      
+      ##
+      # Convert the JSON Schema type into a DataMapper type
+      # 
+      # @todo This should be verified to identify all mysql primitive types 
+      #       and that they map to the correct DataMapper/Ruby types.
+      # 
+      # @param [String] db_type type specified by the database
+      # @param [String] optional format format specification for string attributes
+      # @return [Type] a DataMapper or Ruby type object.
+      # 
+      def get_type(db_type, format = nil)
+        db_type.gsub!(/\(\d*\)/, '')
+        case db_type
+        when 'serial'    then DataMapper::Types::Serial
+        when 'integer'   then Integer
+        when 'number'    then BigDecimal 
+        when 'number'    then Float      
+        when 'boolean'   then DataMapper::Types::Boolean
+        when 'string'    then 
+          case format
+            when nil         then String
+            when 'date-time' then DateTime
+            when 'date'      then Date
+            when 'time'      then Time
+          end
+        end
+      end
+      ##
+      # Get the list of schema names
+      # 
+      # @return [String Array] the names of the schemas in the server.
+      # 
+      def get_storage_names
+        @schemas = self.get_schema
+        @schemas.map { |schema| schema['id'] }
       end
 
-      def fetch_attributes(table)
+      ##
+      # Get the attribute specifications for a specific schema
+      # 
+      # @todo Consider returning actual DataMapper::Properties from this. 
+      #       It would probably require passing in a Model Object.
+      # 
+      # @param [String] table the name of the schema to get attribute specifications for
+      # @return [Hash] the column specs are returned in a hash keyed by `:name`, `:field`, `:type`, `:required`, `:default`, `:key`
+      # 
+      def get_properties(table)
         results = Array.new
-        schema = JSON.parse(self.get_schema(table))
+        schema = self.get_schema(table)[0]
         schema['properties'].each_pair do |key, value|
-          results << ReflectedAttribute.new(key, TypeParser.parse(value['type']))
+          property = {:name => key, :type => get_type(value['type'], value['format']) }
+          property.merge!({ :required => !value['optional'], 
+                         :default => value['default'], 
+                         :key => value.has_key?('index') && value['index'] }) unless property[:type] == DataMapper::Types::Serial
+          results << property
         end
         return results
       end
-    end
-  end
-end
+      
+    end # module PersevereAdapter
+  end # module Reflection
+end # module DataMapper
--- lib/dm-reflection/adapters/postgres.rb
+++ lib/dm-reflection/adapters/postgres.rb
@@ -1,18 +1,59 @@
-module Databases
-  module Postgres
-    
-    def fetch_models
-      query = "select relname from pg_stat_user_tables WHERE schemaname='public'"
-      self.select(query)
-    end
-    
-    def fetch_attributes(table)
-      results = Array.new
-      repository(:defaults).adapter.send(:query, 'PRAGMA table_info(?)', table).each do |column|
-        results << ReflectedAttribute.new(column.name, parse_type(column.type), column.notnull, column.dflt_value, column.pk)
-      end
-      return results
-    end
+module DataMapper
+  module Reflection
+    ##
+    # @todo The postgres adapter extensions have not been tested yet.
+    # 
+    module PostgresAdapter
 
-  end
-end
+      ##
+      # Convert the database type into a DataMapper type
+      # 
+      # @todo This should be verified to identify all mysql primitive types 
+      #       and that they map to the correct DataMapper/Ruby types.
+      # 
+      # @param [String] db_type type specified by the database
+      # @return [Type] a DataMapper or Ruby type object.
+      # 
+      def get_type(db_type)
+        db_type.gsub!(/\(\d*\)/, '')
+        {
+          'INTEGER'           =>  Integer    ,
+          'VARCHAR'           =>  String     ,
+          'NUMERIC'           =>  BigDecimal ,
+          'DOUBLE PRECISION'  =>  Float      ,
+          'DATETIME'          =>  DateTime   ,
+          'DATE'              =>  Date       ,
+          'BOOLEAN'           =>  Types::Boolean  ,
+          'TEXT'              =>  Types::Text
+          }[db_type]
+      end
+
+      ##
+      # Get the list of table names
+      # 
+      # @return [String Array] the names of the tables in the database.
+      # 
+      def get_storage_names
+        self.select("select relname from pg_stat_user_tables WHERE schemaname='public'")
+      end
+
+      ##
+      # Get the column specifications for a specific table
+      # 
+      # @todo Consider returning actual DataMapper::Properties from this. 
+      #       It would probably require passing in a Model Object.
+      # 
+      # @param [String] table the name of the table to get column specifications for
+      # @return [Hash] the column specs are returned in a hash keyed by `:name`, `:field`, `:type`, `:required`, `:default`, `:key`
+      # 
+      def get_properties(table)
+        results = Array.new
+        repository(:defaults).adapter.send(:query, 'PRAGMA table_info(?)', table).each do |column|
+          results << {:name => column.name.downcase, :field => column.name, :type => get_type(column.type), :nullable => column.notnull, :default_value => column.dflt_value, :key => column.pk}
+        end
+        return results
+      end
+      
+    end # module PostgresAdapter
+  end # module Reflection
+end # module DataMapper
