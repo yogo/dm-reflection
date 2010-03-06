d8138c61d93f955844f9bfd4f5b822e7a251898d
Dan Kubb <dan.kubb@gmail.com>
2010-03-06 12:51:25 +0100

    Updated PostgreSQL reflection
    * Added more types
    * Identify primary keys
    * Identify serial keys
    * Set many property options (and more is possible with further wor)

--- lib/dm-reflection/adapters/postgres.rb
+++ lib/dm-reflection/adapters/postgres.rb
@@ -15,17 +15,18 @@ module DataMapper
       # @return [Type] a DataMapper or Ruby type object.
       # 
       def get_type(db_type)
-        db_type.gsub!(/\(\d*\)/, '')
         {
-          'INTEGER'           =>  Integer    ,
-          'VARCHAR'           =>  String     ,
-          'NUMERIC'           =>  BigDecimal ,
-          'DOUBLE PRECISION'  =>  Float      ,
-          'DATETIME'          =>  DateTime   ,
-          'DATE'              =>  Date       ,
-          'BOOLEAN'           =>  Types::Boolean  ,
-          'TEXT'              =>  Types::Text
-          }[db_type]
+          'integer'                     =>  Integer        ,
+          'varchar'                     =>  String         ,
+          'character varying'           =>  String         ,
+          'numeric'                     =>  BigDecimal     ,
+          'double precision'            =>  Float          ,
+          'datetime'                    =>  DateTime       ,
+          'date'                        =>  Date           ,
+          'boolean'                     =>  Types::Boolean ,
+          'text'                        =>  Types::Text    ,
+          'timestamp without time zone' =>  DateTime       ,
+        }[db_type] || raise("unknown db type: #{db_type}")
       end
 
       ##
@@ -34,7 +35,27 @@ module DataMapper
       # @return [String Array] the names of the tables in the database.
       # 
       def get_storage_names
-        self.select("select relname from pg_stat_user_tables WHERE schemaname='public'")
+        select(<<-SQL.compress_lines)
+          SELECT "relname"
+            FROM "pg_stat_user_tables"
+           WHERE "schemaname" = 'public'
+        SQL
+      end
+
+      # Returna list of columns in the primary key
+      #
+      # @param [String] the table name
+      #
+      # @return [Array<String>] the names of the columns in the primary key
+      #
+      def get_primary_keys(table)
+        select(<<-SQL.compress_lines, table).to_set
+              SELECT "key_column_usage"."column_name"
+                FROM "information_schema"."table_constraints"
+          INNER JOIN "information_schema"."key_column_usage" USING("constraint_schema", "constraint_name")
+               WHERE "table_constraints"."constraint_type" = 'PRIMARY KEY'
+                 AND "table_constraints"."table_name" = ?
+        SQL
       end
 
       ##
@@ -47,11 +68,57 @@ module DataMapper
       # @return [Hash] the column specs are returned in a hash keyed by `:name`, `:field`, `:type`, `:required`, `:default`, `:key`
       # 
       def get_properties(table)
-        results = Array.new
-        repository(:defaults).adapter.send(:query, 'PRAGMA table_info(?)', table).each do |column|
-          results << {:name => column.name.downcase, :field => column.name, :type => get_type(column.type), :nullable => column.notnull, :default_value => column.dflt_value, :key => column.pk}
+        columns = select(<<-SQL.compress_lines, schema_name, table)
+            SELECT "column_name"
+                 , "data_type"
+                 , "column_default"
+                 , "is_nullable"
+                 , "character_maximum_length"
+                 , "numeric_precision"
+              FROM "information_schema"."columns"
+             WHERE "table_schema" = ?
+               AND "table_name" = ?
+          ORDER BY "ordinal_position"
+        SQL
+
+        primary_keys = get_primary_keys(table)
+
+        columns.map do |column|
+          type     = get_type(column.data_type)
+          default  = column.column_default
+          required = column.is_nullable == 'NO'
+          key      = primary_keys.include?(column.column_name)
+          length   = column.character_maximum_length
+
+          if type == Integer && default
+            if key
+              type    = DataMapper::Types::Serial if default[/\Anextval/]
+              default = nil
+            else
+              default = default.to_i
+            end
+          end
+
+          if length
+            length = (required ? 1 : 0)..length
+          end
+
+          attribute = {
+            :name     => column.column_name.downcase,
+            :type     => type,
+            :required => required,
+            :default  => default,
+            :key      => key,
+            :length   => length,
+          }
+
+          # TODO: use the naming convention to compare the name vs the column name
+          unless attribute[:name] == column.column_name
+            attribute[:field] = column.column_name
+          end
+
+          attribute
         end
-        return results
       end
       
     end # module PostgresAdapter
