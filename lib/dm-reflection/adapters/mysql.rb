a06189b3a897bb0c1e33796fde86b7edac42598e
irjudson <irjudson@gmail.com>
2010-06-07 15:57:05 -0600

    Reflect mysql relationships out into the meta-code.

--- lib/dm-reflection/adapters/mysql.rb
+++ lib/dm-reflection/adapters/mysql.rb
@@ -59,6 +59,23 @@ module DataMapper
       end
 
       ##
+      # This method breaks the join table into the two other table names
+      #
+      # @param [String] Name join table name
+      # @return [String,String] The two other table names joined.
+      # 
+      def join_table_name(name, name_list=nil)
+        name_list = get_storage_names.sort if name_list.nil?
+        left = name_list[name_list.index(name)-1]
+        right = name[left.length+1..-1]
+        if name_list.include?(right)
+          return left,right
+        else
+          return nil,nil
+        end
+      end
+
+      ##
       # Get the column specifications for a specific table
       #
       # @todo Consider returning actual DataMapper::Properties from this.
@@ -69,8 +86,15 @@ module DataMapper
       #
       def get_properties(table)
         # TODO: use SHOW INDEXES to find out unique and non-unique indexes
+        join_table = false
+        columns = select("SHOW COLUMNS FROM #{table} IN #{options[:path][1..-1]};")
 
-        select("SHOW COLUMNS FROM #{table} IN #{options[:path][1..-1]};").map do |column|
+        if columns.length == 2 && columns[0].field.downcase[-3,3] == "_id" && columns[1].field.downcase[-3,3] == "_id"
+          left_table_name,right_table_name = join_table_name(table)
+          join_table = true
+        end
+        
+        columns.map do |column|
           type           = get_type(column.type)
           auto_increment = column.extra == 'auto_increment'
 
@@ -88,24 +112,35 @@ module DataMapper
             :key      => column.key == 'PRI',
           }
           
-          if type == Integer && field_name[-3,3] == "_id"
+          # TODO: use the naming convention to compare the name vs the column name
+          unless attribute[:name] == column.field
+            attribute[:field] = column.field
+          end
+          
+          if join_table
+            attribute[:type] = DataMapper::Associations::Relationship
+            attribute[:relationship] = {
+              # M:M requires we wire things a bit differently and remove the join model
+              :many_to_many => true,
+              :parent => Extlib::Inflection.classify(left_table_name), 
+              :child => Extlib::Inflection.classify(right_table_name), 
+              # When we can detect more from the database we can optimize this
+              :cardinality => Infinity, 
+              :bidirectional => true }      
+              return [attribute]      
+          elsif type == Integer && field_name[-3,3] == "_id"
             # This is a foriegn key. So this model belongs_to the other (_id) one.
             # Add a special set of values and flag this as a relationship so the reflection code
             # can rebuild the relationship when it's building the model.
             attribute[:type] = DataMapper::Associations::Relationship
             attribute[:relationship] = { 
+              :many_to_many => false,
               :parent => Extlib::Inflection.classify(field_name[0..-4]), 
               :child => Extlib::Inflection.classify(table), 
               # When we can detect more from the database we can optimize this
               :cardinality => Infinity, 
               :bidirectional => true }
           end
-          
-          # TODO: use the naming convention to compare the name vs the column name
-          unless attribute[:name] == column.field
-            attribute[:field] = column.field
-          end
-
           attribute
         end
       end
